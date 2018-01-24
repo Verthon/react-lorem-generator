8e3f872aa497fd2dfd4ab7d744535435761fc044
Verthon <krzysiekksordyl@gmail.com>
2018-01-24 22:59:43 +0100

    changed axios in Output.js

--- src/Output/Output.js
+++ src/Output/Output.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Output.css';
+import axios from 'axios';
 
 class Output extends Component{
   constructor(){
@@ -8,7 +9,15 @@ class Output extends Component{
       paragraphs: [],
     }
   }
-
+  componentDidMount(){
+    axios.get('https://baconipsum.com/api/?type=all-meat')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
   render(){
     return(
       <article id="generated-text" className="App-output">
