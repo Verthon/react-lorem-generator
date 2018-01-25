dd51142f0dd90db583113c72efb9a41564ee3da9
Verthon <krzysiekksordyl@gmail.com>
2018-01-25 20:13:36 +0100

    Added manual changing function for API call

--- src/App.js
+++ src/App.js
@@ -3,33 +3,57 @@ import './App.css';
 import Input from './Input/Input.js';
 import Output from './Output/Output.js';
 import Select from './Select/Select';
+import axios from 'axios'
 
 class App extends Component {
   state = {
-    pValue: 1,
-    html: false
+    pValue: 5,
+    html: 'text',
+    items: []
   }
 
-  pargraphsHandler = (event) => {
+  //Checking the user input of how many paragraphs
+  paragraphsHandler = (event) => {
     this.setState ({
       pValue: event.target.value
     });
-    console.log(event.target.value);
   }
+
+  //Checking the html tags
   htmlTagsHandler = (event) =>{
     console.log(`${event.target.value}`)
+    let userInput = event.target.value;
+    if(userInput === 'yes'){
+      this.setState({
+        html: 'html',
+      })
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`https://baconipsum.com/api/?type=all-meat&paras=${this.state.pValue}&format=${this.state.html}`)
+    .then(response => {
+      this.setState({
+        items: response.data
+      })
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
   
   render() {
+    let items = this.state.items;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">React Lorem Ipsum Generator</h1>
         </header>
         <main className="App-body">
-          <Input changed={this.pargraphsHandler}/>
+          <Input changed={this.paragraphsHandler}/>
           <Select changed={this.htmlTagsHandler}/>
-          <Output/>
+          <Output>{items}</Output>
         </main>  
       </div>
     );
--- src/Output/Output.js
+++ src/Output/Output.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Output.css';
-import axios from 'axios';
+  
+const Output = () => {
 
-class Output extends Component{
-  constructor(){
-    super();
-    this.state = {
-      paragraphs: [],
-    }
-  }
-  componentDidMount(){
-    axios.get('https://baconipsum.com/api/?type=all-meat')
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-  render(){
     return(
       <article id="generated-text" className="App-output">
+        
       </article>
     );
+
   }
-}
 
 export default Output;
